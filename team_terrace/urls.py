aad7a4017816da7d4c9149d58d79afb0c3234c1c
yatashin <s-yata@creationline.com>
2025-10-19 13:47:30 +0900

    app nameがチーム名と一致していなかったため修正

--- team_terrace/urls.py
+++ team_terrace/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "team_c"
+app_name = "team_terrace"
 urlpatterns = [
     path('', views.index, name='index'),
     path('items/', views.items, name='items'),

bfd4690e9039df498705e408c3903689522c4bad
yatashin <s-yata@creationline.com>
2025-10-19 14:24:05 +0900

    conflict test

--- pbl_project/views.py
+++ pbl_project/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     teams = [
-        # {"name": "Team A", "url": "/team-a/"}
+        {"name": "Team A", "url": "/team-a/"}
+
     ]
     return render(request, "top.html", {"teams": teams})

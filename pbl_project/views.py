49477b2f8ff973c2d510da4312d0fea3b44f7c77
Gafu Matsui <matsui.gafu.pa@tut.jp>
2025-10-20 17:46:25 +0900

    Team UDのmembersを更新

--- pbl_project/views.py
+++ pbl_project/views.py
@@ -5,6 +5,6 @@ def index(request):
         # データサンプル
         # {"name": "Team A", "url": "/team_a/"},
         {"name": "Team Terrace", "url": "/team_terrace/"},  # ← 新チームを追加
-        {"name": "Team UD", "url": "/team_UD/"},
+        {"name": "Team UDuv run python manage.py makemigrations", "url": "/team_UD/"},
     ]
     return render(request, "top.html", {"teams": teams})

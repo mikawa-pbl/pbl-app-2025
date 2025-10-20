0826a68097877c36ee63363a7cbbf7f729cc7adb
JupiterMokusei <89513651+gomagoma7@users.noreply.github.com>
2025-10-20 16:20:47 +0900

    feat: add team link in project portal page

--- pbl_project/views.py
+++ pbl_project/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+
 def index(request):
     teams = [
         # データサンプル
         # {"name": "Team A", "url": "/team_a/"},
         {"name": "Team Terrace", "url": "/team_terrace/"},  # ← 新チームを追加
+        {"name": "Team Tansaibou", "url": "/team_tansaibou/"},
     ]
     return render(request, "top.html", {"teams": teams})

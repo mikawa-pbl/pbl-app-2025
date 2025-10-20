c297500df0d504ddbdc3c2c78b9ddc3f88b7b27a
JupiterMokusei <89513651+gomagoma7@users.noreply.github.com>
2025-10-20 16:59:38 +0900

    fix: the path of project in views.py

--- team_tansaibou/views.py
+++ team_tansaibou/views.py
@@ -4,8 +4,8 @@ from .models import Member
 
 
 def index(request):
-    return render(request, 'teams/team_terrace/index.html')
+    return render(request, 'teams/team_tansaibou/index.html')
 
 def members(request):
     qs = Member.objects.using('team_terrace').all()  # ← team_terrace DBを明示
-    return render(request, 'teams/team_terrace/members.html', {'members': qs})
+    return render(request, 'teams/team_tansaibou/members.html', {'members': qs})

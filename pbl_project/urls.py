0f4065b1f0dc70c9e828839801cb7ca8462a4b99
Rikuto Watanabe <watanabe.rikuto.wi@tut.jp>
2025-10-27 17:02:07 +0900

    Team SCIM のセットアップが完了

--- pbl_project/urls.py
+++ pbl_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('team_terrace/', include('team_terrace.urls')),
+    path('team_scim/', include('team_scim.urls')),
 ]

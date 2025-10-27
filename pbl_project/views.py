bc50bdeacc6c88657d540d7aae1362141f6b3e86
horih <seikanbunsiun.17@gmail.com>
2025-10-27 15:28:15 +0900

    fix wrong format pbl_project/view.py

--- pbl_project/views.py
+++ pbl_project/views.py
@@ -4,7 +4,6 @@ def index(request):
     teams = [
         # データサンプル
         # {"name": "Team A", "url": "/team_a/"},
-        
         {"name": "Team AKB5", "url": "/team_akb5/"},  # ← 新チームを追加
         {"name": "Team Terrace", "url": "/team_terrace/"},  # ← 新チームを追加
     ]

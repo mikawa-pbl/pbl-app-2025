8d0c864a1513f88a82911b1a68b94c9f7616af48
s-yata <s-yata@creationline.com>
2025-11-03 14:15:42 +0900

    同様のappが追加されていたことによるエラーの解決

--- pbl_project/settings.py
+++ pbl_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'team_TMR',
-    'django.contrib.staticfiles',
     'graphics',
     'team_terrace',
     'team_akb5',

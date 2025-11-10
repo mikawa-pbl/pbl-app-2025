5c740ff52e8461fc8df986ec46132979084e0822
yif11 <kinohhanten@gmail.com>
2025-11-10 17:13:35 +0900

    add: データベースにIDを追加

--- team_cake/models.py
+++ team_cake/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class Good(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     # image = models.ImageField(upload_to='goods/')
     price = models.IntegerField()

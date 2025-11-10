50381e4a6b25fa27ea09faf70d28e13dbe709fe3
odakazu66 <nobukazu1022@gmail.com>
2025-11-10 16:54:01 +0900

    誤字修正

--- pbl_project/settings.py
+++ pbl_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'djangwo.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'team_northcliff',
     "team_TMR",

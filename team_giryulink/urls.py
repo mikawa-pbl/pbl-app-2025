f52ced1bd905c3535393f407b024703826189d75
RyujiJackson <ryujikung55@gmail.com>
2025-11-17 16:57:42 +0900

    1st Cycle Homepage + ProductList developed

--- team_giryulink/urls.py
+++ team_giryulink/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = "team_terrace"
+app_name = 'team_giryulink'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('members/', views.members, name='members'),
+    path('product/add/', views.add_product, name='add_product'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/<int:pk>/delete/', views.delete_product, name='delete_product'),
 ]

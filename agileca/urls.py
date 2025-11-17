6957020c72b510b1c78e6d26e0914a0bbdf58420
T-Kitahama <239101237+T-Kitahama@users.noreply.github.com>
2025-11-17 17:39:20 +0900

    その他施設のボタンとページを作成

--- agileca/urls.py
+++ agileca/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('members/', views.members, name='members'),
     path('gikamap/', views.gikamap, name='gikamap'),
     path('gikamap/imc/', views.imc, name='imc'),
+    path('gikamap/secretariat/', views.secretariat, name='secretariat'),
+    path('gikamap/health/', views.health, name='health'),
+    path('gikamap/welfare/', views.welfare, name='welfare'),
+    path('gikamap/library/', views.library, name='library'),
 ]

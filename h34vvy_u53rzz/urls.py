befb43e09857ff388cfeb76a830d3e8172ae1b9f
Rin Iwai <rin@eyrin.jp>
2025-12-08 15:34:27 +0900

    feat: ログイン機能の追加

--- h34vvy_u53rzz/urls.py
+++ h34vvy_u53rzz/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "h34vvy_u53rzz"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
     path("help/", views.help, name="help"),
     path("waiting/<int:entry_id>/", views.waiting_view, name="waiting"),
     path(

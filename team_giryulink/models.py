b9cb8fa3b9464966f7c75250ae263c8d4f1fe6ff
NorthHKun123 <kuai.weixi.ob@tut.jp>
2025-12-15 15:30:08 +0900

    image upload implement/edit pbl_project settings

--- team_giryulink/models.py
+++ team_giryulink/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.db import models
 
+
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -8,10 +9,11 @@ class Member(models.Model):
     def __str__(self):
         return f"{self.last_name} {self.first_name}"
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    price = models.PositiveIntegerField(default=0)
-    image = models.URLField(blank=True)
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
     description = models.TextField(blank=True)
 
     def __str__(self):

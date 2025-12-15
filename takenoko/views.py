fe824325e33dff0132aab8e2ce6fbddf52b8a2e3
ito-lily05 <mcsysp2525@gmail.com>
2025-12-15 14:59:00 +0900

    ポップアップ表示ができるようになった

--- takenoko/views.py
+++ takenoko/views.py
@@ -8,5 +8,20 @@ def members(request):
     qs = Member.objects.using('takenoko').all()  # ← team_terrace DBを明示
     return render(request, 'teams/takenoko/members.html', {'members': qs})
 
+def purchased_items(request):
+    return render(request, 'teams/takenoko/purchased_items.html')
+
+def listing_items(request):
+    return render(request, 'teams/takenoko/listing_items.html')
+
 def product_details(request):
     return render(request, 'teams/takenoko/product_details.html')
+
+def login(request):
+    return render(request, 'teams/takenoko/login.html')
+
+def signup(request):
+    return render(request, 'teams/takenoko/signup.html')
+
+def item_create(request):
+    return render(request, 'teams/takenoko/item_create.html')

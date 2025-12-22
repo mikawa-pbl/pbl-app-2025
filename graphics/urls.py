5fdff136ff41816e034ba6d3e8d4ead17b7bb287
JUGEM <teradraw49@gmail.com>
2025-12-22 20:51:19 +0900

    検索機能の追加

--- graphics/urls.py
+++ graphics/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('members/', views.members, name='members'),
     path('book-reviews/', views.book_reviews, name='book_reviews'),
     path('book-reviews/add/', views.add_book_review, name='add_book_review'),
+    path('search/', views.search_courses, name='search_courses'),
+    path('api/teacher-autocomplete/', views.teacher_autocomplete, name='teacher_autocomplete'),
+    path('api/subject-autocomplete/', views.subject_autocomplete, name='subject_autocomplete'),
 ]

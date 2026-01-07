85a3c8c8a3c6a78db4ebad1a0a4e37db1bfa4cab
Watanabe Satoshi <satoshinabe220@gmail.com>
2026-01-07 22:49:34 +0900

    feat: 必ず失敗するテスト

--- h34vvy_u53rzz/tests.py
+++ h34vvy_u53rzz/tests.py
@@ -1,3 +1,13 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import User, Facility, FacilityAccess
+
+
+class FailTest(TestCase):
+    """テストが失敗したときにCIが異常終了することを確認するテスト"""
+    databases = {'h34vvy_u53rzz'}
+
+    def test_always_fail(self):
+        self.assertEqual(1, 2)

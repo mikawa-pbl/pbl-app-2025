21279013e485b056de1631bbab5e0b656271f1fb
yif11 <145346557+yif11@users.noreply.github.com>
2026-01-19 15:35:33 +0900

    fix

--- team_cake/admin.py
+++ team_cake/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import Good, SOSMessage
 
-class GoodAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'original_price')
-
-class SOSMessageAdmin(admin.ModelAdmin):
-    list_display = ('message', 'created_at', 'is_active')
-    list_filter = ('is_active',)
-
-admin.site.register(Good, GoodAdmin)
-admin.site.register(SOSMessage, SOSMessageAdmin)
+# Register your models here.

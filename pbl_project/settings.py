c1a6dd00be6ff21bf5ab9498b905fa0c8f33e1a3
ItoKaede27 <ito.kaede.xp@tut.jp>
2026-01-19 15:58:04 +0900

    Update settings.py

--- pbl_project/settings.py
+++ pbl_project/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_URL = '/team_TMR/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

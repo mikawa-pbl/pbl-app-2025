7ac5ccf0bfcf0be9a2011b5cf8f2644e64ba7454
Rin Iwai <rin@eyrin.jp>
2026-01-20 12:20:15 +0900

    fix: run.shの修正

--- h34vvy_u53rzz/run.sh
+++ h34vvy_u53rzz/run.sh
@@ -3,5 +3,6 @@
 set -e
 
 uv run manage.py makemigrations h34vvy_u53rzz
-uv run manage.py migrate
+uv run manage.py migrate --database default
+uv run manage.py migrate --database h34vvy_u53rzz
 uv run manage.py runserver
